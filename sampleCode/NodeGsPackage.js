75263e3b068c57a39f6d593e83c2e8848a0a260a
Tsuyoshi Kodama <tsuyoshi.kodama@gmail.com>
2014-01-17 19:34:10 +0900

    backup

--- sampleCode/NodeGsPackage.js
+++ sampleCode/NodeGsPackage.js
@@ -16,10 +16,12 @@ require('module').startMain('./nodegs.js');
 
 require('module').endMain();
 
-function require(path) {
 // この関数の中を、圧縮して難読化したい。
+function require(path) {
 
     require = (function () {
+
+        var module = {};
 
         // start Code.gs package
 
@@ -41,8 +43,11 @@ function require(path) {
             if (typeof global === undefined) {
                 this.global = this;
             }
+
+            global.Buffer = function () {};             // Dummy Constractor.
             global.process = require('./process.js');
             global.console = require('./console.js');
+
             (function () {
                 var timer = require('./timers.js');
                 global.setTimeout = timer.setTimeout;
@@ -51,7 +56,16 @@ function require(path) {
                 global.clearInterval = timer.clearInterval;
             })();
 
-            require('module')._resolveFilename = require('./resolve.js');
+            // Setup path resolver of 'require'
+            (function () {
+                var resolve = require('./resolve.js');
+                var module = require('module');
+                var _resolve = module._resolveFilename;
+
+                module._resolveFilename = function (request, parent) {
+                    return _resolve(resolve(request, parent), parent);
+                }
+            })();
 
         require('module').endMain();
 
@@ -66,12 +80,17 @@ function require(path) {
             return require(path);
         }
         if (node.js) {
-            module.exports = Module;
+            module.exports = require('module');
         }
 
         // end of Code.gs package
 
+        return require;
     })();
 
     return require(path);
+}
+
+if (node.js) {
+    module.exports = require('module');
 }
