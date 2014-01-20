0abc76c1a623b3c3149dc43c27dc95815b874fc4
Tsuyoshi Kodama <tsuyoshi.kodama@gmail.com>
2014-01-20 19:18:22 +0900

    merge files.js into module.js

--- spec/codegs/module-spec.js
+++ spec/codegs/module-spec.js
@@ -1,11 +1,11 @@
 
-var Module = require('../../lib/codegs/module.js');
-
 describe("Module:", function () {
+
+    var Module = require('../../lib/codegs/module.js');
 
     describe("'Module' itself:", function () {
 
-        it("should be main module of codegs package.", function () {
+        xit("should be main module of codegs package.", function () {
             expect(Module).toBe(require('../../lib/codegs'));
         });
 
@@ -37,6 +37,21 @@ describe("Module:", function () {
             });
             it("_load", function () {
                 expect(typeof Module._load).toBe('function');
+            });
+            it("_resolveFilename", function () {
+                expect(typeof Module._resolveFilename).toBe('function');
+            });
+            it("_getDirname", function () {
+                expect(typeof Module._getDirname).toBe('function');
+            });
+            it("_joinPath", function () {
+                expect(typeof Module._joinPath).toBe('function');
+            });
+            it("_findFile", function () {
+                expect(typeof Module._findFile).toBe('function');
+            });
+            it("_findModule", function () {
+                expect(typeof Module._findModule).toBe('function');
             });
 
             it("_files", function () {
@@ -117,6 +132,121 @@ describe("Module:", function () {
         });
     });
 
+    describe("method _getDirname:", function () {
+        it("should return dirname in full-filename.", function () {
+            expect(Module._getDirname('/path/to/file.js')).toBe('/path/to');
+            expect(Module._getDirname('/path/to/file/package.json')).toBe('/path/to/file');
+            expect(Module._getDirname('/package.json')).toBe('/');
+        });
+        it("should return null when '/' is not included.", function () {
+            expect(Module._getDirname('module')).toBe(null);
+        });
+    });
+
+    describe("method _joinPath:", function () {
+        it("should return full-filename.", function () {
+            expect(Module._joinPath('project/src', '../lib/assert.js')).toBe('project/lib/assert.js');
+            expect(Module._joinPath('/project/src', '../lib/assert.js')).toBe('/project/lib/assert.js');
+            expect(Module._joinPath('/project/src/', '../lib/assert.js')).toBe('/project/lib/assert.js');
+        });
+        it("overwrite basedir with requested path starts with slash.", function () {
+            expect(Module._joinPath('a/b/c', '/d/file.js')).toBe('/d/file.js');
+            expect(Module._joinPath('/a/b/c', '/d/file.js')).toBe('/d/file.js');
+            expect(Module._joinPath('/a/b/c/', '/d/file.js')).toBe('/d/file.js');
+        });
+        it("process up/down relative-path.", function () {
+            expect(Module._joinPath('a/b/c', '../../d/./e/../../../f/file.js')).toBe('f/file.js');
+            expect(Module._joinPath('/a/b/c', '../../d/./e/../../../f/file.js')).toBe('/f/file.js');
+            expect(Module._joinPath('/a/b/c/', '../../d/./e/../../../f/file.js')).toBe('/f/file.js');
+        });
+        it("keep trail slash.", function () {
+            expect(Module._joinPath('a/b', '../lib/module/')).toBe('a/lib/module/');
+            expect(Module._joinPath('/a/b', '../lib/module/')).toBe('/a/lib/module/');
+            expect(Module._joinPath('/a/b/', '../lib/module/')).toBe('/a/lib/module/');
+        });
+        it("also take slash as root folder.", function () {
+            expect(Module._joinPath('/', 'lib/module')).toBe('/lib/module');
+            expect(Module._joinPath('/', './lib/module')).toBe('/lib/module');
+
+            function test () {
+                Module._joinPath('/', '../file.js');
+            }
+            expect(test).toThrow();
+        });
+        it("should throw in error cases.", function () {
+            function test1 () {
+                Module._joinPath('a/b', '../../../file.js');
+            }
+            function test2 () {
+                Module._joinPath('/a/b', '../../../file.js');
+            }
+            function test3 () {
+                Module._joinPath('/a/b/', '../../../file.js');
+            }
+            expect(test1).toThrow();
+            expect(test2).toThrow();
+            expect(test3).toThrow();
+        });
+    });
+
+    describe("method _findFile:", function () {
+        var pattarnA = "/pattarn-a/filename";
+        Module.define( "/pattarn-a/filename", function () {});
+        Module.define( "/pattarn-a/filename.js", function () {});
+        Module.define( "/pattarn-a/filename/package.json", function () {});
+        Module.define( "/pattarn-a/filename/index.js", function () {});
+
+        var pattarnB = "/pattarn-b/filename";
+        Module.define( "/pattarn-b/filename.js", function () {});
+        Module.define( "/pattarn-b/filename/package.json", function () {});
+        Module.define( "/pattarn-b/filename/index.js", function () {});
+
+        var pattarnC = "/pattarn-c/filename";
+        Module.define( "/pattarn-c/filename/package.json", function () {});
+        Module.define( "/pattarn-c/filename/index.js", function () {});
+
+        var pattarnD = "/pattarn-d/filename";
+        Module.define( "/pattarn-d/filename/index.js", function () {});
+
+        var pattarnE = "/pattarn-e/filename";
+
+        it("should return /pattarn-a/filename", function () {
+            expect(Module._findFile(pattarnA)).toBe(pattarnA + '');
+        });
+
+        it("should return /pattarn-b/filename.js", function () {
+            expect(Module._findFile(pattarnB)).toBe(pattarnB + '.js');
+        });
+
+        it("should return /pattarn-c/filename/package.json", function () {
+            expect(Module._findFile(pattarnC)).toBe(pattarnC + '/package.json');
+        });
+
+        it("should return /pattarn-d/filename", function () {
+            expect(Module._findFile(pattarnD)).toBe(pattarnD + '/index.js');
+        });
+
+        it("should return null when file not found.", function () {
+            expect(Module._findFile(pattarnE)).toBe(null);
+        });
+    });
+
+    describe("module.exists method", function () {
+        it("should return false when not exist.", function () {
+            var NAME = 'nonExistentFileName';
+            expect(Module.exists(NAME)).toBe(false);
+        });
+        it("should return true when exist.", function () {
+            var NAME1 = 'module',
+                NAME2 = './EXISTS_TEST.js';
+            expect(Module.exists(NAME1)).toBe(true);
+
+            var func = function() { ; };
+            Module.define(NAME2, func);
+            expect(Module.exists(NAME2)).toBe(true);
+        });
+    })
+
     describe("require method called in mainModule(Outside module):", function () {
         // Currentry, no test item.
     });
@@ -187,20 +317,4 @@ describe("Module:", function () {
             });
         });
     });
-
-    describe("module.exists method", function () {
-        it("should return false when not exist.", function () {
-            var NAME = 'nonExistentFileName';
-            expect(Module.exists(NAME)).toBe(false);
-        });
-        it("should return true when exist.", function () {
-            var NAME1 = 'module',
-                NAME2 = './EXISTS_TEST.js';
-            expect(Module.exists(NAME1)).toBe(true);
-
-            var func = function() { ; };
-            Module.define(NAME2, func);
-            expect(Module.exists(NAME2)).toBe(true);
-        });
-    })
 });
