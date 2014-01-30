a5e6195d7185a90b06ad1f2b2944ef564ef0a64d
Tsuyoshi Kodama <tsuyoshi.kodama@gmail.com>
2014-01-30 10:37:25 +0900

    backup

--- test/mock/mockfs-spec.js
+++ test/mock/mockfs-spec.js
@@ -9,7 +9,7 @@ describe("mockfs:", function () {
 
     describe("'mockfs' itself:", function () {
         it("should be constractor.", function () {
-            expect(typeof mockfs).toBe('function');
+            expect(typeof MockFs).toBe('function');
         });
     });
 
@@ -23,13 +23,23 @@ describe("mockfs:", function () {
             expect(fs.existsSync('/project/package.json/')).toBe(false);
         });
 
-        it("should return true when directory exists.", function () {
-            var fs = new MockFs({
-                    '/project' : { type: 'dir' }
-                });
+        describe("should return true when directory exists:", function () {
+            it("case#1: stored path has not trairing slash.", function () {
+                var fs = new MockFs({
+                        '/project' : { type: 'dir' }
+                    });
 
-            expect(fs.existsSync('/project')).toBe(true);
-            expect(fs.existsSync('/project/')).toBe(true);
+                expect(fs.existsSync('/project')).toBe(true);
+                expect(fs.existsSync('/project/')).toBe(true);
+            });
+            it("case#2: stored path has trairing slash.", function () {
+                var fs = new MockFs({
+                        '/project/' : { type: 'dir' }
+                    });
+
+                expect(fs.existsSync('/project')).toBe(true);
+                expect(fs.existsSync('/project/')).toBe(true);
+            });
         });
 
         it("should return false when file/directory not exists.", function () {
@@ -39,6 +49,42 @@ describe("mockfs:", function () {
 
             expect(fs.existsSync('/project')).toBe(false);
             expect(fs.existsSync('/project/')).toBe(false);
+        });
+    });
+
+    describe("Method Stat:", function () {
+        it("should return new Stat object.", function () {
+            var fs = new MockFs({
+                    '/test' : { type: 'file' }
+                });
+
+            var stat = new MockFs(fs, '/test');
+
+            expect(stat.mockfs).toBe(fs);
+            expect(stat.filename).toBe('/test');
+        });
+    });
+
+    describe("Method statSync:", function () {
+        it("should return stat object when target exists.", function () {
+            var fs = new MockFs({
+                    '/test' : { type: 'dir' }
+                });
+
+            var stat = fs.statSync('/test');
+
+            expect(stat.mockfs).toBe(fs);
+            expect(stat.filename).toBe('/test');
+        });
+
+        it("should return null when target not exists.", function () {
+            var fs = new MockFs({
+                    '/test' : { type: 'dir' }
+                });
+
+            var stat = fs.statSync('/NOTEXISTS');
+
+            expect(stat).toBe(null);
         });
     });
 });
