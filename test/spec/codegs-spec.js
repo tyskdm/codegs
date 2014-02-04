ab11afa115743ce27a9f393ec4bc3471dedd292e
Tsuyoshi Kodama <tsuyoshi.kodama@gmail.com>
2014-02-04 18:52:16 +0900

    backup

--- test/spec/codegs-spec.js
+++ test/spec/codegs-spec.js
@@ -1,3 +1,17 @@
+/*
+    var config = {
+        rootdir:    process.cwd(),
+
+        mainfile:   argv.targets.length === 0 ? './package.json' :
+                    argv.targets.length === 1 ? argv.targets[0] : null,
+
+        source:     argv.source     || [process.cwd()], // default current directory
+        output:     argv.output     || null,
+        core:       argv.core       || './core',
+        nodecore:   argv.nodecore   || './node_core',
+        kernel:     argv.kernel     || null
+    };
+*/
 
 describe("codegs:", function () {
 
@@ -157,8 +171,8 @@ describe("codegs:", function () {
 
             it("case#1 : add files in ./core directory.", function () {
                 var mockfs = new MockFs({
-                    '/project/core/process.js': { type: 'file'},
-                    '/project/core/Buffer.js':  { type: 'file'},
+                    '/project/core/process.js':     { type: 'file'},
+                    '/project/core/Buffer.json':    { type: 'file'},
                 });
 
                 var list = {};
@@ -166,8 +180,8 @@ describe("codegs:", function () {
 
                 expect(err).toBeNull();
                 expect(list).toEqual({
-                    '/project/core/process.js': { type: 'js', path: 'core/process.js' },
-                    '/project/core/Buffer.js' : { type: 'js', path: 'core/Buffer.js' },
+                    '/project/core/process.js':     { type: 'js',   path: 'core/process.js' },
+                    '/project/core/Buffer.json':    { type: 'json', path: 'core/Buffer.json' },
                 });
             });
 
@@ -189,7 +203,7 @@ describe("codegs:", function () {
                 var mockfs = new MockFs({
                     '/project/core/process.js':     { type: 'file'},
                     '/project/core/lib/Buffer.js':  { type: 'file'},
-                    '/project/core/lib/sub/a.js':   { type: 'file'},
+                    '/project/core/lib/sub/a.json': { type: 'file'},
                 });
 
                 var list = {};
@@ -197,12 +211,11 @@ describe("codegs:", function () {
 
                 expect(err).toBeNull();
                 expect(list).toEqual({
-                    '/project/core/process.js':     { type: 'js', path: 'core/process.js' },
-                    '/project/core/lib/Buffer.js':  { type: 'js', path: 'core/lib/Buffer.js' },
-                    '/project/core/lib/sub/a.js':   { type: 'js', path: 'core/lib/sub/a.js' },
+                    '/project/core/process.js':     { type: 'js',   path: 'core/process.js' },
+                    '/project/core/lib/Buffer.js':  { type: 'js',   path: 'core/lib/Buffer.js' },
+                    '/project/core/lib/sub/a.json': { type: 'json', path: 'core/lib/sub/a.json' },
                 });
-
-            });
+           });
 
             it("case#4 : do nothing when directory not exists.", function () {
                 var mockfs = new MockFs({
@@ -294,24 +307,188 @@ describe("codegs:", function () {
         });
 
         describe("Method addSourceFiles:", function () {
+            // TODO: should check the directory exists or not.
 
+            it("case#1 : add all files in ./ directory.", function () {
+                var mockfs = new MockFs({
+                    '/project/modules/tool.js':     { type: 'file'},
+                    '/project/modules/util.js':     { type: 'file'},
+                    '/project/index.js':            { type: 'file'},
+                });
+                var code = codegs.create();
+                expect(code.setup({
+                        rootdir:    '/project',
+                        mainfile:   'index.js',
+                        source:     ['./'],
+                    }, mockfs)).toBeNull();
+
+                var err = code.addSourceFiles(mockfs);
+                expect(err).toBeNull();
+                expect(code.files.source).toEqual({
+                    '/project/modules/tool.js':     { type: 'js', path: '/modules/tool.js' },
+                    '/project/modules/util.js':     { type: 'js', path: '/modules/util.js' },
+                    '/project/index.js':            { type: 'js', path: '/index.js' },
+                });
+            });
+
+            it("case#2 : add files only in ./lib ./bin directory.", function () {
+                var mockfs = new MockFs({
+                    '/project/modules/tool.js':     { type: 'file'},
+                    '/project/lib/module.js':       { type: 'file'},
+                    '/project/bin/cli.js':          { type: 'file'},
+                    '/project/index.js':            { type: 'file'},
+                });
+                var code = codegs.create();
+                expect(code.setup({
+                        rootdir:    '/project',
+                        mainfile:   'index.js',
+                        source:     ['lib/', './bin'],
+                    }, mockfs)).toBeNull();
+
+                var err = code.addSourceFiles(mockfs);
+                expect(err).toBeNull();
+                expect(code.files.source).toEqual({
+                    '/project/bin/cli.js':          { type: 'js', path: '/bin/cli.js' },
+                    '/project/lib/module.js':       { type: 'js', path: '/lib/module.js' },
+                });
+            });
         });
     });
 
-/*
-    var config = {
-        rootdir:    process.cwd(),
 
-        mainfile:   argv.targets.length === 0 ? './package.json' :
-                    argv.targets.length === 1 ? argv.targets[0] : null,
+    /*
+     *  Method compile:
+     *  - get filename from codegs object.
+     *  - read and wrap source files, and merge them.
+     *  - store that into codegs.content.
+     */
+    describe("Method compile:", function () {
 
-        source:     argv.source     || [process.cwd()], // default current directory
-        output:     argv.output     || null,
-        core:       argv.core       || './core',
-        nodecore:   argv.nodecore   || './node_core',
-        kernel:     argv.kernel     || null
-    };
-*/
+        describe("Static Private Method _compileFilesList:", function () {
+
+            it("should wrap files and return merged one.", function () {
+                var mockfs = new MockFs({
+                    '/project/core/module.js':       { type: 'file', content: '// ## module.js ##\n' },
+                    '/project/core/dom.json':        { type: 'file', content: '// ## dom.json ##\n' },
+                    '/project/main.js':              { type: 'file', content: '// ## main.js ##\n' },
+                    '/project/core/kernel/core.js':  { type: 'file', content: '// ## core.js ##\n' },
+                });
+                var filesList = {
+                    '/project/core/module.js':       { type: 'js',   path: 'core/module.js' },
+                    '/project/core/dom.json':        { type: 'json', path: 'core/dom.json' },
+                    '/project/main.js':              { type: 'main', path: '/main.js' },
+                    '/project/core/kernel/core.js':  { type: 'core', path: 'core/kernel/core.js' },
+                };
+
+                var content = codegs._compileFilesList(filesList, mockfs);
+                expect(typeof content).toBe('string');
+                expect(content).not.toBe('');
+            });
+
+            it("should return null when filetype is invalid.", function () {
+                var mockfs = new MockFs({
+                    '/project/core/module.js':       { type: 'file', content: '// ## module.js ##\n' },
+                });
+                var filesList = {
+                    '/project/core/module.js':       { type: 'INVALID',   path: 'core/module.js' },
+                };
+
+                expect(function () {
+                    codegs._compileFilesList(filesList, mockfs);
+                }).toThrow();
+            });
+        });
+
+        it("should wrap corefile, merge, and store into codegs.content.", function () {
+            var mockfs = new MockFs({
+                '/project/core/module.js':       { type: 'file', content: '// ## module.js ##\n' },
+                '/project/core/dom.json':        { type: 'file', content: '// ## dom.json ##\n' },
+                '/project/index.js':             { type: 'file', content: '// ## index.js ##\n' },
+                '/module.js':                    { type: 'file', content: '// ## module.js ##\n' },
+            });
+            var code = codegs.create();
+            expect(code.setup({
+                    rootdir:    '/project',
+                    mainfile:   'index.js',
+                    kernel:     '/module.js',
+                }, mockfs)).toBeNull();
+            code.files.core = {
+                '/project/core/module.js':       { type: 'js',   path: 'core/module.js' },
+                '/project/core/dom.json':        { type: 'json', path: 'core/dom.json' },
+            };
+
+            var error = code.compile(mockfs);
+            expect(error).toBeNull();
+            expect(typeof code.content).toBe('string');
+            expect(code.content).not.toBe('');
+        });
+
+        it("should wrap node_core files and merge them into codegs.content.", function () {
+            var mockfs = new MockFs({
+                '/project/node_core/assert.js': { type: 'file', content: '// ## assert.js ##\n' },
+                '/project/node_core/util.json': { type: 'file', content: '// ## util.json ##\n' },
+                '/project/index.js':            { type: 'file', content: '// ## index.js ##\n' },
+                '/module.js':                    { type: 'file', content: '// ## module.js ##\n' },
+            });
+            var code = codegs.create();
+            expect(code.setup({
+                    rootdir:    '/project',
+                    mainfile:   'index.js',
+                    kernel:     '/module.js',
+                }, mockfs)).toBeNull();
+            code.files.node_core = {
+                '/project/node_core/assert.js': { type: 'js',   path: 'node_core/assert.js' },
+                '/project/node_core/util.json': { type: 'json', path: 'node_core/util.json' },
+            };
+
+            var error = code.compile(mockfs);
+            expect(error).toBeNull();
+            expect(typeof code.content).toBe('string');
+            expect(code.content).not.toBe('');
+        });
+
+        it("should wrap sourcefile, merge, and store into codegs.content.", function () {
+            var mockfs = new MockFs({
+                '/project/lib/module.js':       { type: 'file', content: '// ## module.js ##\n' },
+                '/project/bin/cli.js':          { type: 'file', content: '// ## cli.js ##\n' },
+                '/project/index.js':            { type: 'file', content: '// ## index.js ##\n' },
+                '/module.js':                    { type: 'file', content: '// ## module.js ##\n' },
+            });
+            var code = codegs.create();
+            expect(code.setup({
+                    rootdir:    '/project',
+                    mainfile:   'index.js',
+                    kernel:     '/module.js',
+                }, mockfs)).toBeNull();
+            code.files.source = {
+                '/project/bin/cli.js':          { type: 'js', path: '/bin/cli.js' },
+                '/project/lib/module.js':       { type: 'js', path: '/lib/module.js' },
+            };
+
+            var error = code.compile(mockfs);
+            expect(error).toBeNull();
+            expect(typeof code.content).toBe('string');
+            expect(code.content).not.toBe('');
+        });
+
+        it("should wrap mainfile, merge, and store into codegs.content.", function () {
+            var mockfs = new MockFs({
+                '/project/index.js':            { type: 'file', content: '// ## index.js ##\n' },
+                '/module.js':                    { type: 'file', content: '// ## module.js ##\n' },
+            });
+            var code = codegs.create();
+            expect(code.setup({
+                    rootdir:    '/project',
+                    mainfile:   'index.js',
+                    kernel:     '/module.js',
+                }, mockfs)).toBeNull();
+
+            var error = code.compile(mockfs);
+            expect(error).toBeNull();
+            expect(typeof code.content).toBe('string');
+            expect(code.content).not.toBe('');
+        });
+    });
 
 
     xdescribe("Method _parsePackageJson:", function () {
