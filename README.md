cc710eaeaeac671c2bf7bcee34f3fbcc8dc85ace
Tsuyoshi Kodama <tsuyoshi.kodama@gmail.com>
2014-02-06 23:52:28 +0900

    add explanation to README.

--- README.md
+++ README.md
@@ -4,8 +4,11 @@ A tool to pack Node.js module files and module system emulator within one source
 That source file can run on [Google Apps Script](https://developers.google.com/apps-script/) environment or perhaps other Javascript engines.
 
 ### Status
-* Basic functionality working.
-* Now still coding and debugging.
+* Basic functions are working.
+* Still coding and debugging.
+
+Code.gs is developed based on Node.js version 0.10.24
+
 
 ## First example
 
@@ -27,7 +30,6 @@ module.exports.sum = function (a, b) {
 ```
 
 those are valid Node.js modules.
-So you can run:
 
 ```shell
 $ node main.js
@@ -106,7 +108,7 @@ $ npm install -g codegs
 $ code mainfile [ options ]
 ```
 
-Code.gs needs some information to work.
+Code.gs needs some information to work. Options let code.gs know that information.
 
 
 ### Minimum info to work
@@ -121,7 +123,7 @@ Code.gs needs some information to work.
 [Current version] specify in command line.<br/>
 [Futher version] command line or `package.json`.
 
-Javascript VM load and run this file first. and against GAS specification, this module will be put in global scope.
+- Javascript VM load and run this file first. and against GAS specification, this module will be put in global scope.
 It should be relative to the root of project directory.
 
 **memo**<br/>
@@ -134,7 +136,7 @@ GAS entry functions should be defined in global scope with `function` statement.
 [Current version] Current working directory.<br/>
 [Futher version] Or directory of `package.json` specified by command line.
 
-Code.gs needs file path information to pack files for `require()` function. But full path name(may include personal name) is not suitable to publish on GAS.For this, Code.gs handle Project directory as a root(/).
+- Code.gs needs file path information to pack files for `require()` function. But full path name(may include personal name) is not suitable to publish on GAS.For this, Code.gs handle Project directory as a root(/).
 
 
 #### Source files or directory
@@ -142,18 +144,18 @@ Code.gs needs file path information to pack files for `require()` function. But 
 [Current version] Command line option `-s`/`--source` or project directory.<br/>
 [Futher version] Or specified in `package.json`.
 
-One or more files/directories are able to set. Those files should be under he project directory.<br/>
-If not set by commandline option, Code.gs use project directory as source directory.
+- One or more files/directories are able to set. Those files should be under he project directory.
+- If not set by commandline option, Code.gs use project directory as source directory.
 
 
 #### Output file
 
-[Current version] Command line option `-o`/`--output` or `stdout`.
+[Current version] Command line option `-o` / `--output` or `stdout`.
 
-Not set, Code.gs outputs to standard output.
+- Not set, Code.gs outputs to standard output.
 
 
-### Advanced options
+### Advanced Info to do more
 
 * node_modules
 * Core modules
@@ -162,33 +164,79 @@ Not set, Code.gs outputs to standard output.
 
 #### node_modules
 
-[Current version] No option. All modules in `PROJECT_DIRECTORY/node_modules/` will be packed.<br/>
-[Futher version] Or specified in `package.json`.
+[Current version] No option. All modules in `PROJECT/node_modules/` will be packed.<br/>
+[Futher version] Not all, only specified modules in `package.json` will be packed.
 
-Code.gs can handle node_modules and module directory. For loading modules inside node_modules directory, no need to code. Both:
-`require('/node_modules/argv')` and `require('argv')` do work.
+- Code.gs can handle node_modules.
 
-Currently, there's no way to ignore node_modules. All node_modules folders are packed into output file.
+- To load modules in `/node_modules`, no need to code fully filepath. Both of bellow are valid.
+
+    ```shell
+    var argv = require('/node_modules/argv.js');
+    var argv = require('argv');
+    ```
+
+- Currently, there's no way to ignore node_modules. All files in node_modules directory are packed into output file.
 
 
 #### Core modules
 
-[Current version] PROJECTDIRECTORY/core is default directory. or Any other directory set by option `-c` / `--core'.
+[Current version] PROJECT/core is default directory. or Any other directory set by option `-c` / `--core'.
 
-Core modules are able to load without directory name such as 'assert', 'http' in Node.js.
+- Core modules are able to load without directory name such as 'assert', 'http' in Node.js. It's same as node_modules, but always this takes priority.
 
-Modules in this directory, Code.gs will assume to be core module. These modules will be laid out special path `core/` in output file(ex. `core/assert.js`). Other module can load like berow.
+- Modules in this directory, Code.gs will assume to be core module. These modules will be laid out hidden path in output file.
 
-```javascript
-var assert = require('assert');
-```
+#### NodeCore modules
 
+[Current version] PROJECT/node_core is default directory. or Any other directory set by option `-n` / `--nodecore`.
 
+- As same as Core modules. It's priority is between core module and node_modules.
+
+    Core module > NodeCore modules > node_modules
+
+- This(PROJECT/node_core) is for Node.js core modules. If you want to use Node.js core files such as util, assert, etc. put it in
+this directory.
+
+**note**<br/>
+Node.js core modules have deep dependency each other modules. So It's not so easy to run outside Node.js environment. I'd like to try some modules.
+
+#### Kernel file
+
+[Current version] Default file is 'module.js' in this package. Option `-k` / `--kernel` set it any other file.
+
+- Default kernel(module.js) is minimum & simple module emulator. But using additional module, It's possible to extend kernel functions like global object `process`, `console`, timer functions,.. if you write.
+
+- Code.gs can pack those additional modules with simple default kernel. and, use that package as a new kernel.
+
+    ... This needs more explanation. I'll add later.
 
 
 ## Limitation and Enhancement
 
 ### No Node.js Global Objects.
 
+Node.js has some global objects(http://nodejs.jp/nodejs.org_ja/docs/v0.10/api/globals.html).
+
+But Code.gs provides only one function 'require'. No other global namespace pollution.
+
 
 ### Enhancemnet require to get Global Objects.
+
+Google Apps Script also provides global objects such as Logger, SpreadsheetApp, etc.
+
+'require()' of Code.gs returns global object by name. It means this code returns original Logger object.
+
+```javascript
+var log = require('Logger');
+```
+
+**memo**<br>
+This is for debugging GAS code on Node.js. Using this, It's easy to inject API objects.
+
+When you want to use mock-Logger or mock-spreadsheet,.. write mock and put it in node_modules directory of parent directry.
+
+ex. if your project root dir is `/home/path/to/project/`, then put mock in `/home/path/to/node_module`.
+
+Node.js search that path and find mock.<br/>
+Code.gs doesn't pack that mock(Because it locate outside project.). and require returns REAL objects.
---
+++ test/integrationTest/case-1/out.js
@@ -0,0 +1,400 @@
+require('module').define('core/core/core.js',
+function (exports, require, module, __filename, __dirname) {
+module.exports = __filename + ' : ' + __dirname;
+
+});
+require('module').define('core/core/package.json',
+{
+  "name": "codegs",
+  "version": "0.0.4",
+  "description": "Tool to pack Node.js module files and module system emulator within one source file.",
+  "repository": {
+    "type": "git",
+    "url": "https://github.com/tyskdm/codegs.git"
+  },
+  "author": {
+    "name": "Tsuyoshi Kodama",
+    "email": "tsuyoshi.kodama@gmail.com"
+  },
+  "license": "MIT",
+  "main": "./core.js",
+  "bin": {
+    "code": "./bin/code"
+  },
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "dependencies": {
+    "argv": "0.0.2",
+    "minimatch": "~0.2.14"
+  }
+}
+);
+require('module').define('node_core/node_core.js',
+function (exports, require, module, __filename, __dirname) {
+module.exports = __filename + ' : ' + __dirname;
+});
+require('module').define('/node_modules/mod/mod.js',
+function (exports, require, module, __filename, __dirname) {
+
+});
+require('module').define('/node_modules/node_modules.js',
+function (exports, require, module, __filename, __dirname) {
+module.exports = __filename + ' : ' + __dirname;
+});
+require('module').define('/lib/sub.js',
+function (exports, require, module, __filename, __dirname) {
+module.exports = __filename + ' : ' + __dirname;
+});
+require('module').runMain('/lib/main.js');
+
+
+var core = require('core');
+var node_core = require('node_core');
+var node_modules = require('node_modules');
+var sub = require('./sub.js');
+
+
+Logger.log( '=====\n' +
+            core + '\n' +
+            node_core + '\n' +
+            node_modules + '\n' +
+            sub + '\n'
+        );
+
+require('module').endMain('/lib/main.js');
+/**
+ * module.js
+ */
+function require(path) {
+
+    require = (function () {
+
+        var global = global || this;
+
+        function Module(id, parent) {
+            this.id = id;
+            this.exports = {};
+            this.parent = parent;
+            if (parent && parent.children) {
+                parent.children.push(this);
+            }
+
+            this.filename = null;
+            this.loaded = false;
+            this.children = [];
+        }
+
+        Module._files = {};
+        Module._cache = {};
+        Module._mainModule = new Module('.', null);
+
+        Module.prototype.require = function (path) {
+            return Module._load(path, this);
+        };
+
+        Module.define = function (filename, func) {
+            Module._files[filename] = func;
+        };
+
+        Module.runMain = function (filename) {
+            Module._mainModule.filename = filename;
+            Module._cache[filename] = Module._mainModule;
+        };
+
+        Module.endMain = function (filename) {
+            if (Module._mainModule.filename === filename) {
+                Module._mainModule.loaded = true;
+            }
+        };
+
+        Module._load = function(path, parent) {
+
+            // Code.gs Specification: require(undefined) should return with nop.
+            if (path === undefined) return null;
+
+            // Code.gs Specification: require(GlobalObject) should return the object.
+            // (but if same-named core module exists, the object will be blocked.)
+            var obj = Module._NativeModule.getObject(path);
+            if (obj) return obj;
+
+            var filename = Module._resolveFilename(path, parent);
+
+            var cachedModule = Module._cache[filename];
+            if (cachedModule) {
+                return cachedModule.exports;
+            }
+
+            var module = new Module(filename, parent);
+            Module._cache[filename] = module;
+
+            var hadException = true;
+            try {
+                module.load(filename);
+                hadException = false;
+            } finally {
+                if (hadException) {
+                    delete Module._cache[filename];
+                }
+            }
+
+            return module.exports;
+        };
+
+        Module._resolveFilename = function (request, parent) {
+
+            if (Module._NativeModule.exists(request)) {
+                return Module._NativeModule.getFilename(request);
+            }
+
+            var filename;
+            var start = request.substring(0, 2);
+
+            if (start === './' || start === '..') {
+                filename = Module._getDirname(parent.filename);
+                filename = Module._joinPath(filename, request);
+                filename = Module._findFile(filename);
+
+            } else if (request.charAt(0) === '/') {
+                filename = Module._findFile(request);
+
+            } else {
+                filename = Module._getDirname(parent.filename);
+                filename = Module._findModule(filename, request, parent);
+            }
+
+            if (!filename) {
+                var err = new Error("Cannot find module '" + request + "'");
+                err.code = 'MODULE_NOT_FOUND';
+                throw err;
+            }
+            return filename;
+        };
+
+        Module._getDirname = function (filename) {
+
+            if (filename === null) return null;
+
+            var n = filename.lastIndexOf('/');
+            if (n < 0) {
+                return null;
+            }
+            if (n === 0) {
+                return '/';
+            }
+            return filename.substring(0, n);
+        };
+
+        Module._joinPath = function (basedir, request) {
+
+            if ((request.charAt(0) === '/') || (basedir === null)) {
+                return request;
+            }
+
+            var req_parts = request.split('/');
+
+            var base_parts = basedir.split('/'),
+                isAbsolute = basedir.charAt(0) === '/';
+
+            if (isAbsolute) base_parts.shift();
+            if (basedir.substr(-1) === '/') base_parts.pop();
+
+            for (var i = 0; i < req_parts.length; i++) {
+                switch (req_parts[i]) {
+                case '.':
+                    break;
+                case '..':
+                    if (base_parts.length === 0) {
+                        throw new Error;
+                    }
+                    base_parts.pop();
+                    break;
+                default:
+                    base_parts.push(req_parts[i]);
+                }
+            }
+
+            return (isAbsolute ? '/' : '') + base_parts.join('/');
+        };
+
+        Module._findFile = function (filename) {
+
+            if (Module._files[filename]) {
+                return filename;
+            }
+
+            var ext = '.js';
+            if (filename.slice(-1) !== '/') {
+                if (Module._files[filename + ext]) {
+                    return filename + ext;
+                }
+            }
+
+            filename += (filename.slice(-1) !== '/') ? '/' : '';
+
+            var file = 'package.json';
+            if (Module._files[filename + file]) {
+                try {
+                    var json = Module._files[filename + file];
+                    var main = json.main;
+                } catch(e) {
+                    return null;
+                }
+                if (typeof main !== 'string') return null;
+
+                file = Module._joinPath(filename, main);
+                file = Module._findFile(file);
+                return file;
+            }
+
+            var file = 'index.js';
+            if (Module._files[filename + file]) {
+                return filename + file;
+            }
+
+            return null;
+        };
+
+        Module._findModule = function (dirname, request, parent) {
+
+            if (dirname === null) return null;
+
+            var option = [ '/node_modules/' ];
+            var filename, dir,
+                dir_parts = (dirname === '/') ? [''] : dirname.split('/');
+
+            for (var i = dir_parts.length; i > 0; i--) {
+                dir = dir_parts.join('/');
+                for (var j = 0; j < option.length; j++) {
+                    filename = Module._findFile(dir + option[j] + request);
+                    if (filename !== null) {
+                        return filename;
+                    }
+                }
+                dir_parts.pop();
+            }
+
+            return null;
+        };
+
+        Module._NativeModule = {
+
+            filePaths: ['core/', 'node_core/'],
+
+            exists: function (request) {
+                var paths = this.filePaths;
+                for (var i = 0; i < paths.length; i++) {
+                    //if (Module._files[paths[i] + request] !== undefined) {
+                    if (Module._findFile(paths[i] + request)) {
+                        return true;
+                    }
+                }
+                return false;
+            },
+
+            getFilename: function (request) {
+                var filename, paths = this.filePaths;
+                for (var i = 0; i < paths.length; i++) {
+                    //if (Module._files[paths[i] + request] !== undefined) {
+                    //    return (paths[i] + request);
+                    //}
+                    filename = Module._findFile(paths[i] + request);
+                    if (filename) {
+                        return filename;
+                    }
+                }
+                return null;
+            },
+
+            getObject: function (request) {
+                if (this.exists(request)) {
+                    return null;
+                }
+                if (global[request] !== undefined) {
+                    return global[request];
+                }
+                return null;
+            }
+        };
+
+        Module.prototype.load = function(filename) {
+            this.filename = filename;
+
+            var code = Module._files[filename];
+
+            if (typeof code === 'object') {
+                this.exports = code;
+                this.loaded = true;
+                return;
+            }
+
+            var self = this;
+            var dirname = Module._getDirname(filename);
+
+            function require(path) {
+                return self.require(path);
+            }
+            require.main = Module._mainModule;
+            require.cache = Module._cache;
+            require.resolve = function(request) {
+                return Module._resolveFilename(request, self);
+            };
+
+            var args = [self.exports, require, self, filename, dirname];
+            code.apply(self.exports, args);
+
+            this.loaded = true;
+        };
+
+        Module.wrap = function (content, filename, filetype) {
+            var wrapper = {
+                    'js':   {   header: "require('module').define('" + filename + "',\n" +
+                                        "function (exports, require, module, __filename, __dirname) {\n",
+                                footer: "\n});\n" },
+
+                    'json': {   header: "require('module').define('" + filename + "',\n",
+                                footer: "\n);\n" },
+
+                    'main': {   header: "require('module').runMain('" + filename + "');\n",
+                                footer: "\nrequire('module').endMain('" + filename + "');\n" },
+
+                    'core': {   header: "function require(path) { require = (function() {\n",
+                                footer: "\nreturn require; })(); return require(path); }\n" }
+                };
+
+            if (wrapper[filetype] === undefined) {
+                return null;
+            }
+
+            return wrapper[filetype].header + content + wrapper[filetype].footer;
+        };
+
+        Module.__reset__ = function () {
+
+            Module._files = {};
+            Module._cache = {};
+            Module._mainModule = new Module('.', null);
+
+            Module.define('core/module', function(exports, require, module, __filename, __dirname) {
+                module.exports = Module;
+            });
+        };
+
+
+        Module.define('core/module', function(exports, require, module, __filename, __dirname) {
+            module.exports = Module;
+        });
+
+        Module._require = function (path) {
+            return Module.prototype.require.call(Module._mainModule, path);
+        };
+
+        return Module._require;
+    })();
+
+    return require(path);
+}
+
+if ((typeof module !== 'undefined') && module.exports) {
+    module.exports = require('module');
+}
