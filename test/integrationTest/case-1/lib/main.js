979be36d9406f957b92d5df8c86bdf7d425ff743
Tsuyoshi Kodama <tsuyoshi.kodama@gmail.com>
2014-02-06 17:54:50 +0900

    add files to integrationTest/case-1.

---
+++ test/integrationTest/case-1/lib/main.js
@@ -0,0 +1,14 @@
+
+
+var core = require('core');
+var node_core = require('node_core');
+var node_modules = require('node_modules');
+var sub = require('./sub.js');
+
+
+Logger.log( '=====\n' +
+            core + '\n' +
+            node_core + '\n' +
+            node_modules + '\n' +
+            sub + '\n'
+        );
