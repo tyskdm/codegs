90511ecbf54fe67eedd176a67c7dadbacf563957
Tsuyoshi Kodama <tsuyoshi.kodama@gmail.com>
2014-02-13 23:09:57 +0900

    call assert(true/false) functions and log results.

--- example/02-UsingCoreModules/src/main.js
+++ example/02-UsingCoreModules/src/main.js
@@ -2,7 +2,16 @@
 require('global');
 
 
+var assert = require('assert');
 
+try {
+    assert(true);
+} catch (e) {
+    console.log('assert(true) throw = ' + e);
+}
 
-
-
+try {
+    assert(false);
+} catch (e) {
+    console.log('assert(false) throw = ' + e);
+}
