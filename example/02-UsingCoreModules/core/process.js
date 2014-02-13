a378a8187afee632797114b111741fd0d2cfd22e
Tsuyoshi Kodama <tsuyoshi.kodama@gmail.com>
2014-02-13 23:08:37 +0900

    Wrap Logger.log in function for convert as process.stdout.write.

--- example/02-UsingCoreModules/core/process.js
+++ example/02-UsingCoreModules/core/process.js
@@ -2,12 +2,19 @@
 var process = {};
 
 var logger = require('Logger');
-process.stdout = { write: logger.log };
+process.stdout = { write: function (data) {
+    logger.log(data);
+    }
+};
 
 process.platform = 'GoogleAppsScript';
 process.pid = 0;
 
 process.env = {};
 process.env.NODE_DEBUG = false;
+
+Error.captureStackTrace = function (THIS, stackStartFunction) {
+    console.log('Error.captureStackTrace called');
+};
 
 module.exports = process;
